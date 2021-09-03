80bb57b430a1c7cb3fc87318194f07059be41b74
Preston Davis <prem.dav@outlook.com>
2021-09-03 11:09:33 -0500

    working on getting location services set up on app

---
+++ src/App.js
@@ -0,0 +1,348 @@
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import {
+  Alert,
+  Button,
+  Linking,
+  PermissionsAndroid,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Switch,
+  Text,
+  ToastAndroid,
+  View,
+} from 'react-native';
+import { PERMISSIONS, check, request, RESULTS } from 'react-native-permissions';
+import Geolocation from 'react-native-geolocation-service';
+import VIForegroundService from '@voximplant/react-native-foreground-service';
+
+import MapView from './MapView';
+import appConfig from '../app.json';
+
+export default function App() {
+  const [forceLocation, setForceLocation] = useState(true);
+  const [highAccuracy, setHighAccuracy] = useState(true);
+  const [locationDialog, setLocationDialog] = useState(true);
+  const [significantChanges, setSignificantChanges] = useState(false);
+  const [observing, setObserving] = useState(false);
+  const [foregroundService, setForegroundService] = useState(false);
+  const [location, setLocation] = useState(null);
+
+  const watchId = useRef(null);
+
+  useEffect(() => {
+    return () => {
+      removeLocationUpdates();
+    };
+  }, [removeLocationUpdates]);
+
+  const hasPermissionIOS = async () => {
+    const openSetting = () => {
+      Linking.openSettings().catch(() => {
+        Alert.alert('Unable to open settings');
+      });
+    };
+    const status = await Geolocation.requestAuthorization('always');
+
+    if (status === 'granted') {
+      Alert.alert(`For best results, allow location always`,
+      '',
+      [
+        { text: 'Go to settings', onPress: openSetting },
+        { text: 'Do this later', onPress: () => {} },
+      ],);
+      return true;
+    }
+
+    if (status === 'denied') {
+      Alert.alert('Location permission denied');
+    }
+
+    if (status === 'disabled') {
+      Alert.alert(
+        `Please enable location services in settings for app to work`,
+        '',
+        [
+          { text: 'Go to Settings', onPress: openSetting },
+          { text: "Don't Use Location", onPress: () => {} },
+        ],
+      );
+    }
+
+    return false;
+  };
+
+  const openSetting = () => {
+    Linking.openSettings().catch(() => {
+      Alert.alert('Unable to open settings');
+    });
+  };
+
+
+  const hasLocationPermission = async () => {
+    if (Platform.OS === 'ios') {
+      return await hasPermissionIOS();
+    }
+
+    if (Platform.OS === 'android' && Platform.Version < 23) {
+      return true;
+    }
+
+    const hasPermission = await check(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION);
+    console.log('android permission result', hasPermission);
+
+    if (hasPermission === RESULTS.GRANTED) {
+      return true;
+    }
+
+    const status = await request(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION, {
+      type: 'always',
+    });
+
+    if (status === PermissionsAndroid.RESULTS.GRANTED) {
+      Alert.alert('Please allow access to location all the time',
+      '',
+      [
+        { text: 'Go to Settings', onPress: openSetting },
+        { text: 'Do this later', onPress: () => {} },
+      ],);
+      return true;
+    }
+
+    if (status === PermissionsAndroid.RESULTS.DENIED) {
+      ToastAndroid.show(
+        'Location permission denied by user.',
+        ToastAndroid.LONG,
+      );
+    } else if (status === PermissionsAndroid.RESULTS.NEVER_ASK_AGAIN) {
+      ToastAndroid.show(
+        'Location permission revoked by user.',
+        ToastAndroid.LONG,
+      );
+    }
+
+    return false;
+  };
+
+  const getLocation = async () => {
+    const hasPermission = await hasLocationPermission();
+
+    if (!hasPermission) {
+      return;
+    }
+
+    Geolocation.getCurrentPosition(
+      (position) => {
+        setLocation(position);
+        console.log(position);
+      },
+      (error) => {
+        Alert.alert(`Code ${error.code}`, error.message);
+        setLocation(null);
+        console.log(error);
+      },
+      {
+        accuracy: {
+          android: 'high',
+          ios: 'best',
+        },
+        enableHighAccuracy: highAccuracy,
+        timeout: 15000,
+        maximumAge: 10000,
+        distanceFilter: 0,
+        forceRequestLocation: forceLocation,
+        showLocationDialog: locationDialog,
+      },
+    );
+  };
+
+  const getLocationUpdates = async () => {
+    const hasPermission = await hasLocationPermission();
+
+    if (!hasPermission) {
+      return;
+    }
+
+    if (Platform.OS === 'android' && foregroundService) {
+      await startForegroundService();
+    }
+
+    setObserving(true);
+
+    watchId.current = Geolocation.watchPosition(
+      (position) => {
+        setLocation(position);
+        console.log(position);
+      },
+      (error) => {
+        setLocation(null);
+        console.log(error);
+      },
+      {
+        accuracy: {
+          android: 'high',
+          ios: 'best',
+        },
+        enableHighAccuracy: highAccuracy,
+        distanceFilter: 0,
+        interval: 5000,
+        fastestInterval: 2000,
+        forceRequestLocation: forceLocation,
+        showLocationDialog: locationDialog,
+        useSignificantChanges: significantChanges,
+      },
+    );
+  };
+
+  const removeLocationUpdates = useCallback(() => {
+    if (watchId.current !== null) {
+      stopForegroundService();
+      Geolocation.clearWatch(watchId.current);
+      watchId.current = null;
+      setObserving(false);
+    }
+  }, [stopForegroundService]);
+
+  const startForegroundService = async () => {
+    if (Platform.Version >= 26) {
+      await VIForegroundService.createNotificationChannel({
+        id: 'locationChannel',
+        name: 'Location Tracking Channel',
+        description: 'Tracks location of user',
+        enableVibration: false,
+      });
+    }
+
+    return VIForegroundService.startService({
+      channelId: 'locationChannel',
+      id: 420,
+      title: appConfig.displayName,
+      text: 'Tracking location updates',
+      icon: 'ic_launcher',
+    });
+  };
+
+  const stopForegroundService = useCallback(() => {
+    VIForegroundService.stopService().catch((err) => err);
+  }, []);
+
+  return (
+    <View style={styles.mainContainer}>
+      <MapView coords={location?.coords || null} />
+
+      <ScrollView
+        style={styles.container}
+        contentContainerStyle={styles.contentContainer}
+      >
+        <View>
+          <View style={styles.option}>
+            <Text>Enable High Accuracy</Text>
+            <Switch onValueChange={setHighAccuracy} value={highAccuracy} />
+          </View>
+
+          {Platform.OS === 'ios' && (
+            <View style={styles.option}>
+              <Text>Use Significant Changes</Text>
+              <Switch
+                onValueChange={setSignificantChanges}
+                value={significantChanges}
+              />
+            </View>
+          )}
+
+          {Platform.OS === 'android' && (
+            <>
+              <View style={styles.option}>
+                <Text>Show Location Dialog</Text>
+                <Switch
+                  onValueChange={setLocationDialog}
+                  value={locationDialog}
+                />
+              </View>
+              <View style={styles.option}>
+                <Text>Force Location Request</Text>
+                <Switch
+                  onValueChange={setForceLocation}
+                  value={forceLocation}
+                />
+              </View>
+              <View style={styles.option}>
+                <Text>Enable Foreground Service</Text>
+                <Switch
+                  onValueChange={setForegroundService}
+                  value={foregroundService}
+                />
+              </View>
+            </>
+          )}
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button title="Get Location" onPress={getLocation} />
+          <View style={styles.buttons}>
+            <Button
+              title="Start Observing"
+              onPress={getLocationUpdates}
+              disabled={observing}
+            />
+            <Button
+              title="Stop Observing"
+              onPress={removeLocationUpdates}
+              disabled={!observing}
+            />
+          </View>
+        </View>
+        <View style={styles.result}>
+          <Text>Latitude: {location?.coords?.latitude || ''}</Text>
+          <Text>Longitude: {location?.coords?.longitude || ''}</Text>
+          <Text>Heading: {location?.coords?.heading}</Text>
+          <Text>Accuracy: {location?.coords?.accuracy}</Text>
+          <Text>Altitude: {location?.coords?.altitude}</Text>
+          <Text>Altitude Accuracy: {location?.coords?.altitudeAccuracy}</Text>
+          <Text>Speed: {location?.coords?.speed}</Text>
+          <Text>Provider: {location?.provider || ''}</Text>
+          <Text>
+            Timestamp:{' '}
+            {location?.timestamp
+              ? new Date(location.timestamp).toLocaleString()
+              : ''}
+          </Text>
+        </View>
+      </ScrollView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#F5FCFF',
+  },
+  contentContainer: {
+    padding: 12,
+  },
+  option: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingBottom: 12,
+  },
+  result: {
+    borderWidth: 1,
+    borderColor: '#666',
+    width: '100%',
+    padding: 10,
+  },
+  buttonContainer: {
+    alignItems: 'center',
+  },
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    marginVertical: 12,
+    width: '100%',
+  },
+});
