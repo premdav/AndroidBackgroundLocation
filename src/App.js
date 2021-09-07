672e9cd17b1c16ae4e547bc2641afb2395ad1df9
Preston Davis <prem.dav@outlook.com>
2021-09-07 14:58:14 -0500

    small fixes in manifest file. Service starts now after turning off and on

--- src/App.js
+++ src/App.js
@@ -1,216 +1,22 @@
 import React, { useCallback, useEffect, useRef, useState } from 'react';
 import {
-  Alert,
-  Button,
-  Linking,
-  PermissionsAndroid,
   Platform,
   ScrollView,
   StyleSheet,
   Switch,
   Text,
-  ToastAndroid,
   View,
   NativeModules,
   TouchableOpacity,
 } from 'react-native';
-import { PERMISSIONS, check, request, RESULTS } from 'react-native-permissions';
-import Geolocation from 'react-native-geolocation-service';
 const { BackgroundModule } = NativeModules;
-
-// const startForegroundService = async () => {
-//   console.log('STARTING FOREGROUND SERVICE UP');
-//   if (Platform.Version >= 26) {
-//     await VIForegroundService.createNotificationChannel({
-//       id: 'locationChannel',
-//       name: 'Location Tracking Channel',
-//       description: 'Tracks location of user',
-//       enableVibration: false,
-//       importance: 5,
-//     });
-//   }
-
-//   return VIForegroundService.startService({
-//     channelId: 'locationChannel',
-//     id: 420,
-//     title: appConfig.displayName,
-//     text: 'Tracking location updates',
-//     icon: 'ic_launcher',
-//     priority: 2,
-//   });
-// };
 
 export default function App() {
   const [forceLocation, setForceLocation] = useState(true);
   const [highAccuracy, setHighAccuracy] = useState(true);
   const [locationDialog, setLocationDialog] = useState(true);
   const [significantChanges, setSignificantChanges] = useState(false);
-  const [observing, setObserving] = useState(true);
-  const [foregroundService, setForegroundService] = useState(true);
   const [location, setLocation] = useState(null);
-
-  const watchId = useRef(null);
-
-  const hasPermissionIOS = async () => {
-    const openSetting = () => {
-      Linking.openSettings().catch(() => {
-        Alert.alert('Unable to open settings');
-      });
-    };
-    const status = await Geolocation.requestAuthorization('always');
-
-    if (status === 'granted') {
-      Alert.alert(`For best results, allow location always`,
-      '',
-      [
-        { text: 'Go to settings', onPress: openSetting },
-        { text: 'Do this later', onPress: () => {} },
-      ],);
-      return true;
-    }
-
-    if (status === 'denied') {
-      Alert.alert('Location permission denied');
-    }
-
-    if (status === 'disabled') {
-      Alert.alert(
-        `Please enable location services in settings for app to work`,
-        '',
-        [
-          { text: 'Go to Settings', onPress: openSetting },
-          { text: "Don't Use Location", onPress: () => {} },
-        ],
-      );
-    }
-
-    return false;
-  };
-
-  const openSetting = () => {
-    Linking.openSettings().catch(() => {
-      Alert.alert('Unable to open settings');
-    });
-  };
-
-
-  const hasLocationPermission = async () => {
-    if (Platform.OS === 'ios') {
-      return await hasPermissionIOS();
-    }
-
-    if (Platform.OS === 'android' && Platform.Version < 23) {
-      return true;
-    }
-
-    const hasPermission = await check(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION);
-    console.log('android permission result', hasPermission);
-
-    if (hasPermission === RESULTS.GRANTED) {
-      return true;
-    }
-
-    const status = await request(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION, {
-      type: 'always',
-    });
-
-    if (status === PermissionsAndroid.RESULTS.GRANTED) {
-      Alert.alert('Please allow access to location all the time',
-      '',
-      [
-        { text: 'Go to Settings', onPress: openSetting },
-        { text: 'Do this later', onPress: () => {} },
-      ],);
-      return true;
-    }
-
-    if (status === PermissionsAndroid.RESULTS.DENIED) {
-      ToastAndroid.show(
-        'Location permission denied by user.',
-        ToastAndroid.LONG,
-      );
-    } else if (status === PermissionsAndroid.RESULTS.NEVER_ASK_AGAIN) {
-      ToastAndroid.show(
-        'Location permission revoked by user.',
-        ToastAndroid.LONG,
-      );
-    }
-
-    return false;
-  };
-
-  const getLocation = async () => {
-    const hasPermission = await hasLocationPermission();
-
-    if (!hasPermission) {
-      return;
-    }
-
-    Geolocation.getCurrentPosition(
-      (position) => {
-        setLocation(position);
-        console.log(position);
-      },
-      (error) => {
-        Alert.alert(`Code ${error.code}`, error.message);
-        setLocation(null);
-        console.log(error);
-      },
-      {
-        accuracy: { 
-          android: 'high',
-          ios: 'best',
-        },
-        enableHighAccuracy: highAccuracy,
-        timeout: 15000,
-        maximumAge: 10000,
-        distanceFilter: 0,
-        forceRequestLocation: forceLocation,
-        showLocationDialog: locationDialog,
-      },
-    );
-  };
-
-  // const getLocationUpdates = async () => {
-  //   const hasPermission = await hasLocationPermission();
-
-  //   if (!hasPermission) {
-  //     return;
-  //   }
-
-  //   // if (Platform.OS === 'android' && foregroundService) {
-  //   //   await startForegroundService();
-  //   // }
-
-  //   setObserving(true);
-
-  //   watchId.current = Geolocation.watchPosition(
-  //     (position) => {
-  //       setLocation(position);
-  //       console.log(position);
-  //     },
-  //     (error) => {
-  //       setLocation(null);
-  //       console.log(error);
-  //     },
-  //     {
-  //       accuracy: {
-  //         android: 'high',
-  //         ios: 'best',
-  //       },
-  //       enableHighAccuracy: true,
-  //       distanceFilter: 0,
-  //       interval: 7500,
-  //       fastestInterval: 5000,
-  //       forceRequestLocation: true,
-  //       useSignificantChanges: false,
-  //     },
-  //   );
-  // };
-
-  // useEffect(() => {
-  //   if (observing) getLocationUpdates();
-  // }, [observing])
 
   return (
     <View style={styles.mainContainer}>
@@ -279,16 +85,6 @@ export default function App() {
             </>
           )}
         </View>
-        {/* <View style={styles.buttonContainer}>
-          <Button title="Get Location" onPress={getLocation} />
-          <View style={styles.buttons}>
-            <Button
-              title="Start Observing"
-              onPress={getLocationUpdates}
-              disabled={observing}
-            />
-          </View>
-        </View> */}
         <View style={styles.result}>
           <Text>Latitude: {location?.coords?.latitude || ''}</Text>
           <Text>Longitude: {location?.coords?.longitude || ''}</Text>
